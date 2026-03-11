64e214b4ecb7696ea69f33c7569a7313c8ded891
nosteam595 <nosteam@bk.ru>
2026-03-11 20:04:40 +0300

    login

--- src/main/java/nosteam/IdeaProjects/PP_3_1_2_Boot_Security_new/config/SecurityConfig.java
+++ src/main/java/nosteam/IdeaProjects/PP_3_1_2_Boot_Security_new/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/css/**", "/js/**").permitAll()
                         .requestMatchers("/auth/login", "/error").permitAll()
                         .requestMatchers("/users/user/details", "/user/details").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/users/**").hasRole("ADMIN")
