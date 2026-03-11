262def3d9a685725f11d34c70ca7a7696f95805d
nosteam595 <nosteam@bk.ru>
2026-03-11 22:58:48 +0300

    task completed

--- src/main/java/nosteam/IdeaProjects/PP_3_1_2_Boot_Security_new/controllers/UserController.java
+++ src/main/java/nosteam/IdeaProjects/PP_3_1_2_Boot_Security_new/controllers/UserController.java
@@ -1,22 +1,16 @@
 package nosteam.IdeaProjects.PP_3_1_2_Boot_Security_new.controllers;
 
 import jakarta.validation.Valid;
-import nosteam.IdeaProjects.PP_3_1_2_Boot_Security_new.model.Role;
 import nosteam.IdeaProjects.PP_3_1_2_Boot_Security_new.model.User;
 import nosteam.IdeaProjects.PP_3_1_2_Boot_Security_new.security.PersonDetails;
 import nosteam.IdeaProjects.PP_3_1_2_Boot_Security_new.services.RoleService;
 import nosteam.IdeaProjects.PP_3_1_2_Boot_Security_new.services.UserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Controller
 @RequestMapping("/users")
@@ -39,11 +33,8 @@ public class UserController {
     }
 
     @GetMapping("/user/details")
-    public String showUserProfile(@RequestParam(value = "id", required = false) Long id,
-                                  @AuthenticationPrincipal PersonDetails personDetails,
-                                  Model model) {
-        User userToShow = userService.getUserForProfile(personDetails.getUser(), id);
-        model.addAttribute("user", userToShow);
+    public String showUserProfile(@AuthenticationPrincipal PersonDetails personDetails, Model model) {
+        model.addAttribute("user", personDetails.getUser());
         return "userProfile";
     }
 
@@ -61,29 +52,16 @@ public class UserController {
         return "redirect:/users";
     }
 
-    @GetMapping("/edit")
-    public String editUserPage(@RequestParam("id") Long id, Model model) {
-        model.addAttribute("user", userService.getUser(id));
-        model.addAttribute("allRoles", roleService.getAllRoles());
-        return "userEdit";
-    }
-
     @PostMapping("/update")
-    public String updateUser(@ModelAttribute("user") @Valid User user,
-                             BindingResult bindingResult,
-                             @RequestParam(value = "listRoles", required = false) List<Long> roleIds,
-                             Model model) {
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("allRoles", roleService.getAllRoles());
-            return "userEdit";
-        }
+    public String updateUser(@ModelAttribute("user") User user,
+                             @RequestParam(value = "listRoles", required = false) List<Long> roleIds) {
         userService.updateUser(user, roleIds);
         return "redirect:/users";
     }
 
     @PostMapping("/remove")
-    public String removeUser(@ModelAttribute("user") User user) {
-        userService.removeUser(user);
+    public String removeUser(@RequestParam("id") Long id) {
+        userService.removeUser(userService.getUser(id));
         return "redirect:/users";
     }
 }
